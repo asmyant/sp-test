ef9a8d14294eb563eab69d98aca1b5371f444133
Tigr <tigranasmyan@gmail.com>
2023-04-27 01:46:55 +0300

    init

--- src/includes/cart/cart.js
+++ src/includes/cart/cart.js
@@ -3,6 +3,7 @@ const cart = '.js-cart';
 const toggleButton = '.js-cart__toggle';
 
 // Toggle mobile class
-document
-  .querySelector(toggleButton)
-  .addEventListener('click', () => document.querySelector(cart).classList.toggle('active'));
+document.querySelector(toggleButton).addEventListener('click', () => {
+  document.body.classList.toggle('fixed');
+  document.querySelector(cart).classList.toggle('active');
+});
